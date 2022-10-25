7a7937e1e6ccfd815e0e531a51b75af61b53e985
Rdoolz51 <Rdoolz51@aol.com>
2022-10-25 19:29:36 -0400

    basic user route layout

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { User, Comment, Post } = require("../../models");
+
+//get all users
+
+
+
+//get a single user by id
+
+
+
+
+
+//create new user
+
+
+
+
+//Login user
+
+
+
+
+//Logout user
+
+
+
+
+//update single user info by id
+
+
+
+
+
+
+//delete single user by id
