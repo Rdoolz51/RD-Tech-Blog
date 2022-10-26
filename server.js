ad21264d36f6a61a905d2dc7dcae33158a629caa
Rdoolz51 <Rdoolz51@aol.com>
2022-10-25 20:34:26 -0400

    server set up for hbars, once i finish the front end js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const routes = require("./controllers");
+const helpers = require('./utils/helpers');
 
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+
+const session = require("express-session");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
@@ -18,15 +24,15 @@ const sess = {
     }),
 };
 
-app.use(session(sess));
-
-//uncomment when ready to initialize handlebars
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 
 app.use(routes);
 
