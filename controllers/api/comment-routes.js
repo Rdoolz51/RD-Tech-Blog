55e71f72f91fc776301ef7609b040d51ccaf1f8e
Rdoolz51 <Rdoolz51@aol.com>
2022-10-25 20:00:25 -0400

    done with routes, i think?

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,36 +3,102 @@ const { Comment, Post, User } = require("../../models");
 const withAuth = require('../../utils/auth');
 
 //get all comments
-
+router.get("/", (req, res) => {
+    Comment.findAll({
+        attributes: ["id", "comment_text"],
+        include: [
+            {
+                model: Post,
+                attributes: ["id"],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+            {
+                model: User,
+                attributes: ["username"],
+            },
+        ],
+    })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
 //get single comment by id
-
+router.get("/:id", (req, res) => {
+    Comment.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ["id", "comment_text"],
+        ibclude: [
+            {
+                model: Post,
+                attributes: ["id"],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+            {
+                model: User,
+                attributes: ["username"],
+            },
+        ],
+    })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
 
 //create new post
-
+router.post("/", withAuth, (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id,
+    })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
 
 //update a comment
 router.put("/:id", (req, res) => {
-    User.update(req.body, {
-        indivdualHooks: true,
-        where: {
-            id: req.params.id,
+    Comment.update(
+        {
+            comment_text: req.body.comment_text,
         },
-    })
-        .then((dbUserData) => {
-            if (!dbUserData) {
-                res.status(404).json({ message: "There is no user found by this id" });
+        {
+            where: {
+                id: req.params.id,
+            },
+        }
+    )
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
+                res
+                    .status(404)
+                    .json({ message: "No comment found by this id!" });
                 return;
             }
-            res.json(dbUserData);
+            res.json(dbCommentData);
         })
         .catch((err) => {
             console.log(err);
@@ -44,21 +110,20 @@ router.put("/:id", (req, res) => {
 
 
 //delete comment by id
-router.delete("/:id", (req, res) => {
-    User.destroy({
+router.delete("/:id", withAuth, (req, res) => {
+    Comment.destroy({
         where: {
             id: req.params.id,
         },
     })
-        .then((dbUserData) => {
-            //checks to see if actual user exsists
-            if (!dbUserData) {
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
                 res
                     .status(404)
-                    .json({ message: "There was no user found by this id!" });
+                    .json({ message: "No comment found by this id!" });
                 return;
             }
-            res.json(dbUserData);
+            res.json(dbCommentData);
         })
         .catch((err) => {
             console.log(err);
